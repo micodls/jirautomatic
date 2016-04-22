6d5f06551a61247fab9c1cd145f0d206b94f854c
Mico de los Santos <mico.dlsantos@gmail.com>
2016-04-22 16:35:05 +0800

    Test for csv output

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import csv
+
+with open('eggs.csv', 'wb+') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
