66d6f8db313dab971e93c45fb0b79e2b4ec7eb25
Mico de los Santos <mico.dlsantos@gmail.com>
2016-04-29 17:10:11 +0800

    removed unneeded file

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import csv
-
-with open('eggs.csv', 'wb+') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=',')
-    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
